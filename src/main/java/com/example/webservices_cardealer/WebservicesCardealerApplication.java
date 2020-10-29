30426697e6108995cd609d4ce72096d4af72e921
Benjamin E Boson <Benjamineboson@gmail.com>
2020-10-29 10:14:49 +0100

    Init commit

---
+++ src/main/java/com/example/webservices_cardealer/WebservicesCardealerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservices_cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesCardealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesCardealerApplication.class, args);
+    }
+
+}
