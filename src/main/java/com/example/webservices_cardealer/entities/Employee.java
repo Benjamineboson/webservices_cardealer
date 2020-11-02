5cf750239633473f6c28589fb468a9245cf6be9f
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-11-02 17:23:10 +0100

    Validation employee

--- src/main/java/com/example/webservices_cardealer/entities/Employee.java
+++ src/main/java/com/example/webservices_cardealer/entities/Employee.java
@@ -9,12 +9,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
@@ -28,7 +26,11 @@ public class Employee implements Serializable {
     private static final long serialVersionUID = -4711394998133225957L;
     @Id
     private String employeeId;
+    @NotEmpty(message = "Firstname can not be empty")
+    @Size(min = 3, max = 10, message = "Firstname length invalid")
     private String firstname;
+    @NotEmpty(message = "Lastname can not be empty")
+    @Size(min = 3, max = 10 , message = "Firstname length invalid")
     private String lastname;
     @Past
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -36,10 +38,14 @@ public class Employee implements Serializable {
     private LocalDate birthdate;
     @Pattern(regexp = "([0-9]){2,4}-([0-9]){5,8}", message = "Phone number not valid")
     private String phone;
-    @Email
+    @Email(message = "E-mail address invalid")
     private String email;
+    @Size(min = 4, max = 10, message = "Username length invalid")
+    @NotBlank(message = "Username must contain a value")
+    @Field("username")
     @Indexed(unique = true)
     private String username;
+    @Size(min = 4, max = 10, message = "Password length invalid")
     @NotBlank(message = "Password must contain a value")
     @JsonIgnore
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
